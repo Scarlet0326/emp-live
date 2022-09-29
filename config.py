a98f3bd9fe0335ba07054d80ca58dcf7a1d690b7
Scarlet0326 <112604994+Scarlet0326@users.noreply.github.com>
2022-09-29 17:25:20 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "database-1.c0owxjwoh7um.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
-custompass = "Bait3113"
+custompass = "Bait3273"
 customdb = "employee"
 custombucket = "tengjinlun-employee"
 customregion = "us-east-1"
